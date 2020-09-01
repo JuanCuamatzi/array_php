8c917460a7c0156d007982e67150d855e97bf87d
JuanCuamatzi <70585908+JuanCuamatzi@users.noreply.github.com>
2020-09-01 10:34:33 -0500

    Update arreglo.php

--- arreglo.php
+++ arreglo.php
@@ -14,7 +14,18 @@ class Arreglo
    $this->Array[i] = rand();
   }
  }
- public function ordena();
+ public function ordena($Arreglo)
+  for($i=1;$i<sizeof($Arreglo);$i++){
+          for($j=0;$j<sizeof($Arreglo)-$i;$j++){
+              if($Arreglo[$j]>$Arreglo[$j+1]){
+                 $k=$Arreglo[$j+1]; 
+                 $Arreglo[$j+1]=$Arreglo[$j]; 
+                 $Arreglo[$j]=$k;
+              }
+          }
+  }
+      return $Arreglo;
+}
  public function cuenta();
  public function medVar();
  public function mostrar();
